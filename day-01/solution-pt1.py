e07da249e891368d0bb8da4a3652361156559c75
Ben DeVries <me@ben-devries.com>
2023-12-12 17:50:14 -0500

    improvements

--- day-01/solution-pt1.py
+++ day-01/solution-pt1.py
@@ -2,14 +2,14 @@ import re
 
 
 def main():
-    nums = []
+    total = 0
 
-    with open("./input.txt", "r") as f:
+    with open("input.txt") as f:
         for line in f:
             digits = re.findall(r"\d", line)
-            nums.append(int(digits[0] + digits[-1]))
+            total += int(digits[0] + digits[-1])
 
-    print(f"The solution is {sum(nums)}")
+    print(f"The solution is {total}")
 
 
 if __name__ == "__main__":
