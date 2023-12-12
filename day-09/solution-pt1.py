7ad41184ed82ee60305dfb9613c51ec2513803d7
Ben DeVries <me@ben-devries.com>
2023-12-12 18:59:27 -0500

    add d9p1 solution

---
+++ day-09/solution-pt1.py
@@ -0,0 +1,31 @@
+def solve_row(row: list[int]) -> int:
+    diffs = [[*row]]
+
+    while any((n != 0 for n in diffs[-1])):
+        local_diffs = []
+        for a, b in zip(diffs[-1][0:], diffs[-1][1:]):
+            local_diffs.append(b - a)
+        diffs.append(local_diffs)
+
+    for i in range(len(diffs) - 1, 0, -1):
+        diffs[i - 1].append(diffs[i][-1] + diffs[i - 1][-1])
+
+    return diffs[0][-1]
+
+
+def main():
+    sequences = []
+    total = 0
+
+    with open("input.txt") as f:
+        for line in f:
+            sequences.append([int(n) for n in line.split()])
+
+    for seq in sequences:
+        total += solve_row(seq)
+
+    print(f"The solution is {total}")
+
+
+if __name__ == "__main__":
+    main()
